f86ae27902511ebcbe7a6a1505fd2ab19eee7614
gnapahcuna <34328416+gnapahcuna@users.noreply.github.com>
2017-12-07 13:23:36 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -17,10 +17,18 @@ if (!is_null($events['events'])) {
 			$replyToken = $event['replyToken'];
 
 			// Build message to reply back
-			$messages = [
+			if($text=='สวัสดี'){
+				$messages = [
 				'type' => 'text',
-				'text' => 'สาสสส'
-			];
+				'text' => $text
+				];	
+			}else{
+				$messages = [
+				'type' => 'text',
+				'text' => 'ไม่เข้าใจคำถาม'
+				];
+			}
+			
 
 			// Make a POST Request to Messaging API to reply to sender
 			$url = 'https://api.line.me/v2/bot/message/reply';
